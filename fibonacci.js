a7d0b43142695038bae59b21ed6add6c1b15cf77
owenwinner <owenwinner2@gmail.com>
2022-08-26 12:48:23 +0100

    adding fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -8,8 +8,19 @@ console.log(fibonacci(10));
 
 //define array
 let fib_array = [];
+
 //write a FOR LOOP
 for (let i = 0; i <= 10; i++){
     fib_array.push(fibonacci(i));
 }
 console.log(fib_array);
+
+let name = ["winner", "jane", "chi", "boy"]; //name is an array of one element
+let food = ["yam", "beans", "rice", "Mango"]; //food is an array of one element
+let fruit = "Mango"
+for(let x = 0; x <name.length; x++){
+    if(fruit == food[x]){
+        console.log(name[x])
+    }
+
+}
